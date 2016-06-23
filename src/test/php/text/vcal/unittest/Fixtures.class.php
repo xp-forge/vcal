4cb1d15d37b2cf1c2b45c0bca1fe2fe0aaac498f
Timm Friebe <thekid@thekid.de>
2016-06-23 22:43:49 +0200

    Refactor: Extract strings and values to Fixtures enum

---
+++ src/test/php/text/vcal/unittest/Fixtures.class.php
@@ -0,0 +1,145 @@
+<?php namespace text\vcal\unittest;
+
+use text\vcal\Calendar;
+use text\vcal\Event;
+use text\vcal\Organizer;
+use text\vcal\Attendee;
+use text\vcal\Text;
+use text\vcal\Date;
+use text\vcal\TimeZone;
+use text\vcal\TimeZoneInfo;
+
+class Fixtures extends \lang\Enum {
+  public static $calendar, $event, $timezone;
+
+  static function __static() {
+    $i= 0;
+
+    self::$calendar= new self(
+      $i++,
+      'calendar',
+      '
+        BEGIN:VCALENDAR
+        METHOD:REQUEST
+        PRODID:Microsoft Exchange Server 2010
+        VERSION:2.0
+        END:VCALENDAR
+      ',
+      Calendar::with()
+        ->method('REQUEST')
+        ->prodid('Microsoft Exchange Server 2010')
+        ->version('2.0')
+        ->event(null)
+        ->create()
+    );
+
+    self::$event= new self(
+      $i++,
+      'event',
+      '
+        BEGIN:VEVENT
+        ORGANIZER;CN=The Organizer:MAILTO:organizer@example.com
+        ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE;CN=The Attendee 1:MAILTO:attendee1@example.com
+        ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE;CN=The Attendee 2:MAILTO:attendee2@example.com
+        ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE;CN=The Attendee 3:MAILTO:attendee3@example.com
+        COMMENT;LANGUAGE=de-DE:\n
+        SUMMARY;LANGUAGE=de-DE:Treffen
+        DTSTART;TZID=W. Europe Standard Time:20160524T183000
+        DTEND;TZID=W. Europe Standard Time:20160524T190000
+        LOCATION;LANGUAGE=de-DE:BS 50 EG 0102
+        END:VEVENT
+      ',
+      Event::with()
+        ->organizer(Organizer::with()->cn('The Organizer')->value('MAILTO:organizer@example.com')->create())
+        ->attendee([
+          Attendee::with()
+            ->role('REQ-PARTICIPANT')
+            ->partstat('NEEDS-ACTION')
+            ->rsvp('TRUE')
+            ->cn('The Attendee 1')
+            ->value('MAILTO:attendee1@example.com')
+            ->create()
+          ,
+          Attendee::with()
+            ->role('REQ-PARTICIPANT')
+            ->partstat('NEEDS-ACTION')
+            ->rsvp('TRUE')
+            ->cn('The Attendee 2')
+            ->value('MAILTO:attendee2@example.com')
+            ->create()
+          ,
+          Attendee::with()
+            ->role('REQ-PARTICIPANT')
+            ->partstat('NEEDS-ACTION')
+            ->rsvp('TRUE')
+            ->cn('The Attendee 3')
+            ->value('MAILTO:attendee3@example.com')
+            ->create()
+        ])
+        ->dtstart(new Date('W. Europe Standard Time', '20160524T183000'))
+        ->dtend(new Date('W. Europe Standard Time', '20160524T190000'))
+        ->location(new Text('de-DE', 'BS 50 EG 0102'))
+        ->comment(new Text('de-DE', "\n"))
+        ->summary(new Text('de-DE', 'Treffen'))
+        ->create()
+    );
+
+    self::$timezone= new self(
+      $i++,
+      'timezone',
+      '
+        BEGIN:VTIMEZONE
+        TZID:W. Europe Standard Time
+        BEGIN:STANDARD
+        DTSTART:16010101T030000
+        TZOFFSETFROM:+0200
+        TZOFFSETTO:+0100
+        RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=-1SU;BYMONTH=10
+        END:STANDARD
+        BEGIN:DAYLIGHT
+        DTSTART:16010101T020000
+        TZOFFSETFROM:+0100
+        TZOFFSETTO:+0200
+        RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=-1SU;BYMONTH=3
+        END:DAYLIGHT
+        END:VTIMEZONE
+      ',
+      new TimeZone(
+        'W. Europe Standard Time',
+        TimeZoneInfo::with()
+          ->dtstart('16010101T030000')
+          ->tzoffsetfrom('+0200')
+          ->tzoffsetto('+0100')
+          ->rrule('FREQ=YEARLY;INTERVAL=1;BYDAY=-1SU;BYMONTH=10')
+          ->create()
+        ,
+        TimeZoneInfo::with()
+          ->dtstart('16010101T020000')
+          ->tzoffsetfrom('+0100')
+          ->tzoffsetto('+0200')
+          ->rrule('FREQ=YEARLY;INTERVAL=1;BYDAY=-1SU;BYMONTH=3')
+          ->create()
+      )
+    );
+  }
+
+  /**
+   * Creates a new fixture
+   *
+   * @param  int $ordinal
+   * @param  string $name
+   * @param  string $string
+   * @param  var $object
+   */
+  public function __construct($ordinal, $name, $string, $object) {
+    parent::__construct($ordinal, $name);
+    $this->string= $string;
+    $this->object= $object;
+  }
+
+  /** @return string */
+  public function string() { return trim(preg_replace("/\n\s+/", "\n", $this->string)); }
+
+  /** @return var */
+  public function object() { return $this->object; }
+}
--- src/test/php/text/vcal/unittest/VCalFormatTest.class.php
+++ src/test/php/text/vcal/unittest/VCalFormatTest.class.php
@@ -2,12 +2,33 @@
 
 use text\vcal\VCalFormat;
 use lang\FormatException;
+use io\streams\MemoryOutputStream;
 
 class VCalFormatTest extends \unittest\TestCase {
+
+  /** @return php.Generator */
+  private function fixtures() {
+    foreach (Fixtures::values() as $fixture) {
+      yield [$fixture];
+    }
+  }
 
   #[@test]
   public function can_create() {
     new VCalFormat();
+  }
+
+  #[@test, @values('fixtures')]
+  public function read($fixture) {
+    $this->assertEquals($fixture->object(), (new VCalFormat())->read($fixture->string()));
+  }
+
+  #[@test, @values('fixtures')]
+  public function write($fixture) {
+    $out= new MemoryOutputStream();
+    (new VCalFormat())->write($fixture->object(), $out);
+
+    $this->assertEquals($fixture->string(), trim($out->getBytes()));
   }
 
   #[@test, @expect(FormatException::class), @values([
