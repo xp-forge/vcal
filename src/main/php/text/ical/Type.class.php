62f9ba811bbe8b72bbd9ed6e553f954240978659
Timm Friebe <thekid@thekid.de>
2016-06-26 16:37:02 +0200

    Add GROUP and RESOURCE types

--- src/main/php/text/ical/Type.class.php
+++ src/main/php/text/ical/Type.class.php
@@ -4,8 +4,11 @@
  * Attendee type
  *
  * @see  xp://text.ical.Attendee#type
+ * @see  http://tools.ietf.org/html/rfc2445#section-4.2.3
  */
 abstract class Type {
   const INDIVIDUAL = 'INDIVIDUAL';
   const ROOM       = 'ROOM';
+  const GROUP      = 'GROUP';
+  const RESOURCE   = 'RESOURCE';
 }
