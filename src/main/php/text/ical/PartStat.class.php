369c6c4a34c3ca3210bc78feaf572c15548c2633
Timm Friebe <thekid@thekid.de>
2016-06-26 16:39:46 +0200

    Add IN-PROCESS partstat

--- src/main/php/text/ical/PartStat.class.php
+++ src/main/php/text/ical/PartStat.class.php
@@ -4,6 +4,7 @@
  * Attendee participation status
  *
  * @see  xp://text.ical.Attendee#partstat
+ * @see  http://tools.ietf.org/html/rfc2445#section-4.2.12
  */
 abstract class PartStat {
   const NEEDS_ACTION = 'NEEDS-ACTION';
@@ -12,5 +13,6 @@ abstract class PartStat {
   const TENTATIVE    = 'TENTATIVE';
   const DELEGATED    = 'DELEGATED';
   const PARTIAL      = 'PARTIAL';
+  const IN_PROCESS   = 'IN-PROCESS';
   const COMPLETED    = 'COMPLETED';
 }
