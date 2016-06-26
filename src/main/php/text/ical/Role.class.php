ab73336a6db5c508e99e82c4eb8f0967b0461685
Timm Friebe <thekid@thekid.de>
2016-06-26 16:37:56 +0200

    Add 'OPT-PARTICIPANT' role

--- src/main/php/text/ical/Role.class.php
+++ src/main/php/text/ical/Role.class.php
@@ -4,9 +4,11 @@
  * Attendee role
  *
  * @see  xp://text.ical.Attendee#role
+ * @see  http://tools.ietf.org/html/rfc2445#section-4.2.16
  */
 abstract class Role {
   const CHAIR           = 'CHAIR';
   const REQ_PARTICIPANT = 'REQ-PARTICIPANT';
+  const OPT_PARTICIPANT = 'OPT-PARTICIPANT';
   const NON_PARTICIPANT = 'NON-PARTICIPANT';
 }
